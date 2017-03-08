66c3139adaaf4d6a829f241ca3680965428e08d2
Your Name <glidev5@gmail.com>
2017-03-08 18:10:03 +0000

    1.0.0

---
+++ gqdoq.js
@@ -0,0 +1,23 @@
+var Q = require('q');
+
+function doQ(o) {
+    // doQ(o)
+    // in: o.query
+    // out: o.result
+    // promise return: doQ({query:query,...other parameter query needs})
+    o = o || {};
+    var deferred = Q.defer();
+    try {
+        o.query(o, function (e, o2) {
+            o2 = o2 || {};
+            o2.error = e || null;
+            deferred.resolve(o2);
+        });
+    } catch (e) {
+        o.error = e;
+        deferred.resolve(o);
+    }
+    return deferred.promise;
+}
+
+exports.doQ = doQ;
---
+++ test/test.js
@@ -0,0 +1,41 @@
+/**
+ * Created by happy on 3/4/17.
+ */
+var assert=require('assert');
+var doq=require('./../gqdoq.js');
+
+it("should be able to run doq",function(done){
+
+    // add is an o-tree compatible function, o.is the input and cb will callback o once done where result is saved in o.result
+    function add(o,cb){
+        try {
+            o.result = o.a + o.b;
+            cb(null,o);
+        }catch(e){
+            cb(e);
+        }
+    }
+
+    // q_add is a q-tree compatible function for add
+    function q_add(o){
+        o=o||{};
+        o.query=add;
+        return doq.doQ(o);
+    }
+
+    // data o needed by add
+    var o={
+        a:2,
+        b:3
+    };
+
+    // q_add is actually a Q() object with then function available
+    q_add(o).then(function(o){
+        console.log(JSON.stringify(o));
+        assert(o.result== o.a+ o.b);
+        assert(!o.error);
+    }).done(done);// notice you can done(done) in mocha
+
+});
+
+
