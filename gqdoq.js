27708f174a2d5be85e653b95c748d536b5ee5eff
glidev5 <glidev5@gmail.com>
2020-05-24 04:02:03 +0000

    update

--- gqdoq.js
+++ gqdoq.js
@@ -1,4 +1,6 @@
 var Q = require('q');
+var deepExtend=require('deep-extend');
+var async=require('async');
 
 function doQ(o) {
     // doQ(o)
@@ -23,6 +25,14 @@ function doQ(o) {
 exports.doQ=doQ;
 
 function extendQ(func){
+    // extendQ(func)
+    // in:func
+    // out:func
+    // extends func with q method
+    // q(o)
+    // in:o.query
+    // return:o.result
+
     func.q=function(o){
         o=o||{};
         o.query=func;
@@ -32,3 +42,71 @@ function extendQ(func){
 }
 
 exports.extendQ=extendQ;
+
+
+Object.prototype.Q=function(o){
+    // attach doQ method to object
+    doQ(o||this);
+}
+
+Object.prototype.deepExtend=function(obj2){
+    // attach deepExtend method to object
+    deepExtend(this,obj2);
+}
+
+// attach Q to global
+global.Q=Q;
+
+var defaultCB=function(e,r){
+    // default callback when it is not defined
+    if(e){
+        console.error(e);
+    }
+    try{
+        this.result=r;
+    }catch(e){}
+}
+
+Object.prototype.mapLimit=function(limit,iteratee,cb){
+    // attach mapLimit to object. This method intends map from any array. It does not yield key in iteratee
+    // in:
+    // [limit]  maplimit limit concurrent execution, default 16
+    // iteratee   i.e. function(item,cb){}
+    // [cb]     callback, default defaultCB
+    async.mapLimit(this,limit||16,iteratee,cb||defaultCB);
+}
+
+Object.prototype.mapValuesLimit=function(limit,iteratee,cb){
+    // attach mapValuesLimit to object.  This method intends map from any object. It yields key in iteratee
+    // in:
+    // [limit] limit concurrent execution, default 16
+    // iteratee  i.e. function(item,key,cb)
+    // [cb]     callback, default defaultCB
+    async.mapValuesLimit(this,limit||16,iteratee,cb||defaultCB);
+}
+
+Object.prototype.mapSeries=function(iteratee,cb){
+    // attach mapSeries to object. This method intends map from any array. It does not yield key in iteratee
+    // in:
+    // iteratee  i.e. function(item,cb){}
+    // [cb]     callback, default defaultCB
+    async.mapSeries(this,iteratee,cb||defaultCB);
+}
+
+Object.prototype.mapValuesSeries=function(iteratee,cb){
+    // attach mapValuesSeries to object.  This method intends map from any object. It does yield key in iteratee
+    // in:
+    // iteratee  i.e. function(item,key,cb){}
+    // [cb]     callback, default defaultCB
+    async.mapValuesSeries(this,iteratee,cb||defaultCB);
+}
+
+Object.prototype.waterfall=function(cb){
+    // attach waterfall to object.  This method runs a series of functions in an array in waterfall style.
+    // in:
+    // [cb]    callback, default defaultCB
+    async.waterfall(this,cb||defaultCB);
+}
+
+// attach async to global
+global.async=async;
--- test/test.js
+++ test/test.js
@@ -63,3 +63,122 @@ it('should be able to run doq using q extension',function(done){
     }).done(done);// notice you can done(done) in mocha
 
 })
+
+it('should be able to run mapLimit',function(done){
+    var o={a:1,b:2,c:3};
+    o.mapLimit(3,function(item,cb){
+    	cb(null,item);
+    },function(e,r){
+    	var result=JSON.stringify(r);
+    	console.log(result);
+    	assert.equal(result,"[1,2,3]");
+    	done(e);
+    });
+})
+
+it('should be able to run mapValuesLimit',function(done){
+    var o={a:1,b:2,c:3};
+    o.mapValuesLimit(3,function(item,key,cb){
+    	cb(null,"key:"+key+";value:"+item);
+    },function(e,r){
+    	var test={"a":"key:a;value:1","b":"key:b;value:2","c":"key:c;value:3"};
+    	test=JSON.stringify(test);
+    	var result=JSON.stringify(r);
+    	console.log(result);
+    	assert.equal(result,test);
+    	done(e);
+    })
+})
+
+it('should be able to run mapSeries',function(done){
+  var o={a:1,b:2,c:3};
+  o.mapSeries(function(item,cb){
+    cb(null,"value:"+item);
+  },function(e,r){
+    var test=["value:1","value:2","value:3"];
+    test=JSON.stringify(test);
+    var result=JSON.stringify(r);
+    console.log(result);
+    assert.equal(result,test);
+    done(e);
+  })
+})
+
+it('should be able to run mapValuesSeries',function(done){
+  var o={a:1,b:2,c:3};
+  o.mapValuesSeries(function(item,key,cb){
+    cb(null,"key:"+key+";value:"+item);
+  },function(e,r){
+    var test={"a":"key:a;value:1","b":"key:b;value:2","c":"key:c;value:3"};
+    test=JSON.stringify(test);
+    var result=JSON.stringify(r);
+    console.log(result);
+    assert.equal(result,test);
+    done(e);
+  })
+})
+
+it('should be able to run waterfall',function(done){
+  var data=[1,2,3];
+    var o=[
+      function(cb){   // notice first function must have no data
+        data.mapLimit(16,function(item,cb){cb(null,item*2)},function(e,r){cb(e,r)});
+      },
+      function(data,cb){  // subsequent data come from previous cb(e,r) r value
+        data.mapLimit(16,function(item,cb){cb(null,item*2)},function(e,r){cb(e,r,r)});
+      },
+      function(data,data2,cb){  // if you return 2 r, please add 2 data
+        data.mapLimit(16,function(item,cb){cb(null,item*2)},function(e,r){cb(e,r)});
+      }
+    ]
+    o.waterfall(function(e,r){
+      var test=[8,16,24];
+      test=JSON.stringify(test);
+      var result=JSON.stringify(r);
+      console.log(result)
+      assert.equal(result,test);
+      done(e);
+    })
+})
+
+it('should be able to run deepExtend',function(done){
+  var o1={a:1,b:2,c:3,k:{a:1,m:7,n:null}};
+  var o2={d:4,a:7,b:-1,k:{l:3,m:4,n:5}};
+  o1.deepExtend(o2);
+  var test={"a":7,"b":-1,"c":3,"k":{"a":1,"m":4,"n":5,"l":3},"d":4};
+  test=JSON.stringify(test);
+  var result=JSON.stringify(o1);
+  console.log(result)
+  assert.equal(result,test);
+  done();
+})
+
+it('should be able to run global.Q',function(done){
+  function eventually(value) {
+    return Q.delay(value, 10);
+  }
+
+  Q.all([1, 2, 3].map(eventually))
+  .done(function (result) {
+      console.log(result);
+      result=JSON.stringify(result);
+      var test=[ 1, 2, 3 ];
+      test=JSON.stringify(test);
+      assert.equal(test,result);
+      done();
+  });
+})
+
+it('should be able to run global.async',function(done){
+  var o=[1,2,3];
+  async.mapLimit(o,3,function(item,cb){
+    cb(null,item*2);
+  },function(e,r){
+    var test=[2,4,6];
+    test=JSON.stringify(test);
+    var result=JSON.stringify(r);
+    console.log(result)
+    assert.equal(result,test);
+    done();
+  })
+})
